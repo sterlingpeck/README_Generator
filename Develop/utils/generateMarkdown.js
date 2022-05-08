241b69746206c27fa31466e3dbef2ba0bcec8267
Sterling Peck <peck.sterling@gmail.com>
2022-05-08 11:17:12 -0600

    generate markdown content

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -27,4 +27,30 @@ function renderLicenseLink(license) {
   }
 }
 
+// Generate the final markdown content
+function generateMarkdown(data) {
+  return `# ${data.title}
+![License Badge](${renderLicenseBadge(data.license)})
+  
+## Table of Contents
+${generateTableOfContents(data)}
+## Description
+${data.description}
+${generateDeployedLink(data.deployed)}
+## Installation
+${data.installation}
+${generateTests(data.tests)}
+## Usage
+${data.usage}
+${generateContribution(data.contribute)}
+## Questions
+If you have any questions, contact me through [GitHub](https://github.com/${
+    data.github
+  }) or by [sending me an email](mailto:${data.email}).
+## License
+Usage of this application is governed by the [${
+    data.license
+  }](${renderLicenseLink(data.license)}) license.
+`;
+}
 module.exports = generateMarkdown;
