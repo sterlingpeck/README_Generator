c34a3d6f3be40c6075d9efb8204de974380617aa
Sterling Peck <peck.sterling@gmail.com>
2022-05-08 10:35:57 -0600

    markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,125 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on which license is passed in
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "Apache 2.0":
+      return "./assets/images/license-Apache-2-blue.svg";
+    case "GNU General Public License":
+      return "./assets/images/license-GPL-blue.svg";
+    case "MIT":
+      return "./assets/images/license-MIT-blue.svg";
+    default:
+      return "";
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function that returns the license section of README
+function renderLicenseLink(license) {
+  switch (license) {
+    case "Apache 2.0":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "GNU General Public License":
+      return "https://opensource.org/licenses/gpl-license";
+    case "MIT":
+      return "https://opensource.org/licenses/MIT";
+    default:
+      return "";
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+// // Generate Test Info section, if user indicates inclusion of tests
+// const generateTests = (tests) => {
+//   if (!tests) {
+//     return "";
+//   }
+//   return `
+// ## Tests
+// The following tests have been implemented for this application:
+// ${tests}
+// `;
+// };
+
+// Generate Deployment Link section, if user indicates that a deployed version exists
+const generateDeployedLink = (link) => {
+  if (!link) {
+    return "";
+  }
+  return `
+## Deployment
+A deployed version of this application can be found [here](${link}).
+`;
+};
+
+// Generate contribution information
+const generateContribution = (contributionInfo) => {
+  if (!contributionInfo) {
+    return "";
+  }
+
+  if (contributionInfo === "Contributor Covenant") {
+    return `
+## Contribution
+Contribution to this project is governed by the Contributor Covenant's [Code of Conduct](https://www.contributor-covenant.org/version/2/1/code_of_conduct/). Please read through this code and ensure any proposed contributions follow the guidelines set forth therein.
+`;
+  } else {
+    return `
+## Contribution
+  
+${contributionInfo}
+`;
+  }
+};
+
+// Generate table of contents
+const generateTableOfContents = (data) => {
+  let tests = "";
+  let deployed = "";
+  let contribution = "";
+  if (data.confirmTests) {
+    tests = `* [Tests](<#tests>)`;
+  }
+  if (data.confirmDeployed) {
+    deployed = `* [Deployment](<#deployment>)`;
+  }
+  if (data.confirmContribute) {
+    contribution = `* [Contribution](#contribution)`;
+  }
+  return `
+* [Description](#description)
+${deployed}
+* [Installation](#installation)
+${tests}
+* [Usage](#usage)
+${contribution}
+* [Questions](#questions)
+* [License](#license)
+`;
+};
+
+// Generate the final markdown content
 function generateMarkdown(data) {
   return `# ${data.title}
-
+![License Badge](${renderLicenseBadge(data.license)})
+  
+## Table of Contents
+${generateTableOfContents(data)}
+## Description
+${data.description}
+${generateDeployedLink(data.deployed)}
+## Installation
+${data.installation}
+${generateTests(data.tests)}
+## Usage
+${data.usage}
+${generateContribution(data.contribute)}
+## Questions
+If you have any questions, contact me through [GitHub](https://github.com/${
+    data.github
+  }) or by [sending me an email](mailto:${data.email}).
+## License
+Usage of this application is governed by the [${
+    data.license
+  }](${renderLicenseLink(data.license)}) license.
 `;
 }
 
